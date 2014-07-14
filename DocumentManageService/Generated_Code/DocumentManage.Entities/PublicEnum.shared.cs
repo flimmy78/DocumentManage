6339169c2984611d5712d08ec29da5cc3e7aaea6
freewill2009 <334160817@qq.com>
2014-07-14 11:32:26 +0800

    2

---
+++ DocumentManageService/Generated_Code/DocumentManage.Entities/PublicEnum.shared.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManage.Entities
+{
+    [Flags]
+    public enum LogType : short
+    {
+        [Display(Name = "默认")]
+        Default = 0x01,
+        [Display(Name = "系统")]
+        System = 0x02,
+        [Display(Name = "历史")]
+        History = 0x04,
+        [Display(Name = "文档")]
+        Document = 0x08,
+        [Display(Name = "浏览")]
+        View = 0x10
+    }
+}
